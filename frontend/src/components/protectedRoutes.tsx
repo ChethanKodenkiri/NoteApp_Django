925ef45444d73d73986ca80f543d196d03890f72
ChethanKodenkiri <chethankodenkiri@gmail.com>
2024-06-01 00:23:02 +0530

    + Add: Added protected Routes <Signed By:ChethanKodenkiri>

---
+++ frontend/src/components/protectedRoutes.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+import { jwtDecode } from 'jwt-decode';
+import { ACCESS_TOKEN,REFRESH_TOKEN } from '../constants'
+import api from '../api'
+import { Navigate } from 'react-router-dom';
+
+function protectedRoutes({children}:any) {
+   const [isAutherized, setAutherized] = useState<null|Boolean>(null);
+
+   useEffect(()=>{
+    auth().catch(()=>setAutherized(false))
+   },[])
+
+   const refreshToken=async function(){
+    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+    try {
+        const res:any = await api.post('api/token/refresh/',{refresh:refreshToken})
+        if(res.status ==200){
+            localStorage.setItem(ACCESS_TOKEN,res.data.access)
+            setAutherized(true)
+        }
+    } catch (error) {
+        console.log(error)
+        setAutherized(false)
+    }
+   }
+
+   const auth=async function(){
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if(!token){
+        setAutherized(false)
+        return;
+    }
+
+    const tk = jwtDecode(token);
+    const expired:any = tk.exp;
+    const now = Date.now()/1000
+    if(expired<now){
+        await refreshToken();
+    }else{
+        setAutherized(true)
+    }
+   }
+   if (isAutherized == null){<div>Loading...</div>}
+   return isAutherized?children:<Navigate to={'./login'}/>
+}
+
+export default protectedRoutes
